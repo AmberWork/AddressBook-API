4134e494bd4433f5ac850949edb4b53dc5039f10
aldaine_dell <vainedev@gmail.com>
2023-02-14 18:13:43 -0500

    updated root api with router from all other api endpoints

--- apis/root.api.js
+++ apis/root.api.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const commonEndpoints = require("../apis/routes/common.api")
+const webEndpoints = require("../apis/routes/web.api")
+const adminEndpoints = require("../apis/routes/admin.api")
 
 router.use("/common", commonEndpoints);
+router.use("/web", webEndpoints);
+router.use("/admin", adminEndpoints);
 
 
 
