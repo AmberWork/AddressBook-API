8634f558bacce07b9ee323ac65ffc02f445ad169
aldaine_dell <vainedev@gmail.com>
2023-02-16 10:42:39 -0500

    created constants folder and file to store enum values

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -1,20 +1,17 @@
 const {Schema, model} = require("mongoose");
 const bcrypt = require("bcryptjs");
 const addressSchema = require("./address.schema");
+const { roleEnum, statusEnum } = require("../constants/enum.constant");
 
-const roleEnum = [
-    "ADMIN", "USER"
-];
-const statusEnum = [
-    "PENDING", "APPROVED", "ACTIVE", "INACTIVE"
-];
+
+
 const userSchema = new Schema({
     first_name: {type: String, required:[true, "First Name is a required field"]},
     last_name: {type: String, required:[true, "Last Name is a required field"]},
     email: {type: String, required:[true, "Email is a required field"]},
     password: {type: String, required:[true, "Password is a required field"]},
     profile_image: {type: String},
-    role: {type: String, enum:{values:["ADMIN", "USER"], message: `{VALUES} is not a valid role. roles: ${roleEnum}`},  default: "USER"},
+    role: {type: String, enum:{values:roleEnum, message: `{VALUES} is not a valid role. roles: ${roleEnum}`},  default: "USER"},
 
     // The cell number will be required if the home number is not provided and vice versa.
     mobile_number: {type: String, required: [function(){ return this.role != "ADMIN" && !this.home_number}, "A phone number needs to be on file"]},
