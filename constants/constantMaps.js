ad01348bb58437af225fdcf168b79572581bff3d
aldaine_dell <vainedev@gmail.com>
2023-02-20 12:52:52 -0500

    updated the constant file to add method to get key from value in a map

--- constants/constantMaps.js
+++ constants/constantMaps.js
@@ -11,3 +11,19 @@ module.exports.statusEnum = new Map ([
     ["ACTIVE", 2], 
     ["INACTIVE", 3]
 ]);
+
+
+/**
+ * Searches a map for a value then return the key that it is attached to. If no key is value is found it returns null
+ * @param {Map} map the map to search
+ * @param {Number} value value to search for in the map
+ * @returns { String|null }
+ */
+module.exports.getKeyFromValue = (map, value)=>{
+    for(let [key, val] of map.entries()){
+        if(val === value){
+            return key;
+        }
+    }
+    return null;
+}
