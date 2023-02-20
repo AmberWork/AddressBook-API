d50a54f729a9c7f9e4ffdce34ab56b234f4ce7b4
Anita Baker <anitabaker@myambergroup.com>
2023-02-20 07:41:03 -0500

    Admin Routes added

--- apis/v1/routes/admin.api.js
+++ apis/v1/routes/admin.api.js
@@ -1,6 +1,22 @@
+// ---------------
+// Based Routes
+// ---------------
 const router = require("express").Router();
 
+const {
+    getAllUsers,
+    createUser
+} = require("../controllers/user.controller");
+// ---------------
+
+
+// ---------------
+// Based Routers
+// ---------------
+router.route('/users').get(getAllUsers).post(createUser);
+// ---------------
 
 
 
 module.exports = router;
+// ---------------
