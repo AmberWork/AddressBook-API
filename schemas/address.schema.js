4a04314f307502e126a23149e53db67cde088dba
aldaine_dell <vainedev@gmail.com>
2023-02-23 09:27:25 -0500

    changed address schema properties to lowercase

--- schemas/address.schema.js
+++ schemas/address.schema.js
@@ -2,14 +2,14 @@ const {Schema, model} = require("mongoose");
 
 
 const addressSchema = new Schema({
-    Address_1: {type: String, required: [true, "Line 1 of address is required"]},
-    Address_2: {type: String},
+    address_1: {type: String, required: [true, "Line 1 of address is required"]},
+    address_2: {type: String},
     user_id: {type: Schema.Types.ObjectId, ref: "User", required:[true, "An address must be linked to a user"] }, 
     city: {type: String, required: [true, "City of address is required"]},
     parish: {type: Schema.Types.ObjectId, ref: "Parish", required: [true, "Parish of address is required"]},
     status: {type: Number, default:0},
     deletedAt: {type: Date, default: null}
-});
+}, {timestamps: true});
 
 
 
