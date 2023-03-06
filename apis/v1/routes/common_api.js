74b0d5a55d4a63485f8fb1afcaff4e2aeccb8878
Tyrese <jrackfinn@gmail.com>
2023-03-06 06:04:26 -0500

    status approve patch request

--- apis/v1/routes/common_api.js
+++ apis/v1/routes/common_api.js
@@ -1,66 +1,67 @@
 const router = require("express").Router();
 const FileUploader = require("../../../utilities/file_upload_utility");
-const UserController = require("../controllers/user_controller")
-const Middleware = require("../middlewares/middleware")
+const UserController = require("../controllers/user_controller");
+const Middleware = require("../middlewares/middleware");
 const AddressController = require("../controllers/addresses_controller");
-const ParishController = require("../controllers/parish_controller")
-router
-    .route("/login")
-    .post(UserController.loginUser)
-
+const ParishController = require("../controllers/parish_controller");
+router.route("/login").post(UserController.loginUser);
 
 router
-    .all(new Middleware().protectedViaRole(["USER", "ADMIN"]))
-    .route("/getRoleAndStatus")
-    .get(UserController.getRolesAndStatus);
+  .all(new Middleware().protectedViaRole(["USER", "ADMIN"]))
+  .route("/getRoleAndStatus")
+  .get(UserController.getRolesAndStatus);
 
 router
-    .route("/users")
-    .post(FileUploader.uploadFile("Profile").single("profile_image"),UserController.createUser)
+  .route("/users")
+  .post(
+    FileUploader.uploadFile("Profile").single("profile_image"),
+    UserController.createUser
+  );
 
 router
-    .route("/users/:user_id")
-    .all(new Middleware().protectedViaRole(["ADMIN", "USER"]))
-    .get(UserController.getUserById)
-    .patch(FileUploader.uploadFile("Profile").single("profile_image"),UserController.updateUser)
-    .delete(UserController.deleteUser)
+  .route("/users/:user_id")
+  .all(new Middleware().protectedViaRole(["ADMIN", "USER"]))
+  .get(UserController.getUserById)
+  .patch(
+    FileUploader.uploadFile("Profile").single("profile_image"),
+    UserController.updateUser
+  )
+  .delete(UserController.deleteUser);
 
-
-    // Password
-router.route("/requestPasswordReset")
-    .all(new Middleware().protectedViaRole(["USER","ADMIN"]))
-    .post(UserController.requestPasswordReset);
-
-router.route("/resetPassword")
-    .post(UserController.resetPassword);
-    
-
-    // address
+// Password
 router
-    .route('/addresses')
-    .all(new Middleware().protectedViaRole(["USER","ADMIN"]))
-    .post(AddressController.createAddress)
+  .route("/requestPasswordReset")
+  .all(new Middleware().protectedViaRole(["USER", "ADMIN"]))
+  .post(UserController.requestPasswordReset);
 
-    // Admins should also be able to go to this route
+router.route("/resetPassword").post(UserController.resetPassword);
+
+// address
 router
-    .route('/addresses/:id')
-    .all(new Middleware().protectedViaRole(["USER","ADMIN"]))
-    .get(AddressController.getAddressById)
-    .put(AddressController.updateAddress)
-    .delete(AddressController.softDeleteAddress);
+  .route("/addresses")
+  .all(new Middleware().protectedViaRole(["USER", "ADMIN"]))
+  .post(AddressController.createAddress);
 
+// Admins should also be able to go to this route
+router
+  .route("/addresses/:id")
+  .all(new Middleware().protectedViaRole(["USER", "ADMIN"]))
+  .get(AddressController.getAddressById)
+  .patch(AddressController.statusChange)
+  .put(AddressController.updateAddress)
+  .delete(AddressController.softDeleteAddress);
 
-router.route('/addresses/:id/destroy')
-    .all(new Middleware().protectedViaRole(["USER","ADMIN"]))
-    .delete(AddressController.destroyAddress);
+router
+  .route("/addresses/:id/destroy")
+  .all(new Middleware().protectedViaRole(["USER", "ADMIN"]))
+  .delete(AddressController.destroyAddress);
 
-router.route("/parishes")
-    .all(new Middleware().protectedViaRole(["USER", "ADMIN"]))
-    .get(ParishController.getAllParish)
-    .get(ParishController.getParishByName);
+router
+  .route("/parishes")
+  .all(new Middleware().protectedViaRole(["USER", "ADMIN"]))
+  .get(ParishController.getAllParish)
+  .get(ParishController.getParishByName);
 
+router.route("parishes/:id").get(ParishController.getParishById);
 
-router.route("parishes/:id")
-    .get(ParishController.getParishById)
- 
 module.exports = router;
